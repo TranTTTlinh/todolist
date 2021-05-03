401999e969acdefd5327e9e21b9ebccf424e21f9
TranTTTlinh <linh.tran@stunited.vn>
2021-05-03 22:52:39 +0700

    dat commit source

--- app/Http/Controllers/Todo/TodoController.php
+++ app/Http/Controllers/Todo/TodoController.php
@@ -21,6 +21,11 @@ class TodoController extends Controller
         }
     }
 
+    /**
+     * Undocumented function
+     * đạt chỉnh sửa ở đây
+     * @return void
+     */
     public function add_todo() {
         $this->AuthLogin();
         $typeTodo = DB::table('typetodolist')->get();
